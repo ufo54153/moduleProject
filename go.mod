7d52ca274e1ab68bc3e6e0aaf3d139f6a14e91c2
ArcturusZhang <dapzhang@microsoft.com>
2019-10-30 15:12:35 +0800

    Move to v2

--- go.mod
+++ go.mod
@@ -3,6 +3,6 @@ module github.com/ArcturusZhang/moduleProject
 go 1.13
 
 require (
-	github.com/ArcturusZhang/module-demo-test/services/aznet/mgmt/2019-10-01/aznet v1.0.1
-	github.com/ArcturusZhang/module-demo-test/services/bznet/mgmt/2019-11-01/bznet v1.0.1
+	github.com/ArcturusZhang/module-demo-test/services/aznet/mgmt/2019-10-01/aznet v1.0.2
+	github.com/ArcturusZhang/module-demo-test/services/bznet/mgmt/2019-11-01/bznet/v2 v2.0.0
 )
--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"fmt"
 	"github.com/ArcturusZhang/module-demo-test/services/aznet/mgmt/2019-10-01/aznet"
-	"github.com/ArcturusZhang/module-demo-test/services/bznet/mgmt/2019-11-01/bznet"
+	"github.com/ArcturusZhang/module-demo-test/services/bznet/mgmt/2019-11-01/bznet/v2"
 )
 
 func main() {
@@ -11,6 +11,6 @@ func main() {
 	azClient := aznet.AzNetsClient{}
 	fmt.Println(azClient.Get())
 	// Use Bznet
-	bzClient := bznet.BzNetClient{}
+	bzClient := bznet.BzNetsClient{}
 	fmt.Println(bzClient.Get())
 }
